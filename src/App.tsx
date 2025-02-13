60c51b351896b2a9efd8e5814cf8020a2a65ce4e
Kwesi Abraham <kwesiabraham4503@gmail.com>
2025-02-13 03:33:35 +0000

    Update App.tsx: Setting routes using router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from "./components/Home.tsx";
-import { BrowserRouter } from "react-router-dom";
-
+import BodyMassIndex from "./components/BMI.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BMI from './components/BMI.tsx';
 function App() {
   return (
-    <div>
-     <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bmi" element={<BMI />} />
+      </Routes>
+    </Router>
   );
 }
 
