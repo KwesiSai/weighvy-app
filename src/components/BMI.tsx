e41a6e7ba7047ae0c5986da1b47482b46acc0319
Kwesi Abraham <kwesiabraham4503@gmail.com>
2025-02-13 05:11:29 +0000

    update feature/BMI: edit styling of form element

--- src/components/BMI.tsx
+++ src/components/BMI.tsx
@@ -6,23 +6,40 @@ const MainContainer = styled.div`
     display:grid;
     grid-template-columns: 1fr 1fr;
     margin-top: 10%;
+    margin-left: -5%;
 `
 const LogoSection = styled.section`
     display: flex;
     flex-direction: column;
     padding: 0 0 0 10%;
-    margin-left: 0;
 
     img{
         height: auto;
-        width: 100%;
+        width: 90%;
     }
 `
 const CalculateSection = styled.section`
-    margin-left: 20%;
+    margin-left: 15%;
+    margin-right: 5%;
+    border: 1px solid black;
+    form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 4%;
+    }
+    form input{
+         height: 30px;
+         width: 60%;
+         margin-bottom: 7%;
+    }
+         form label{
+            font-weight: 100;
+            margin-bottom: 3%;
+         }
 `
 const DisplayContainer = styled.div`
-
+    justify-self: center;
 `
 
 const  BMI: React.FC = () => {
@@ -33,7 +50,10 @@ const  BMI: React.FC = () => {
             </LogoSection>
             <CalculateSection>
                 <form>
-                    this is my form
+                    <label for="height"> Enter your height in M</label>
+                    <input type="number" id="height" name="height"/>
+                    <label for="weight">Enter your weight in KG</label>
+                    <input type="number" id="weight" name="height"/>
                 </form>
                 <DisplayContainer>
                     something to display
